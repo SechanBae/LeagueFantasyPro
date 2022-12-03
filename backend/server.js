32e709860da4aee01731178d6224c584f41fcc5d
SechanBae <sechan.bae@gmail.com>
2022-12-03 18:35:41 -0500

    Revert "Update server.js"
    This reverts commit 9a3eeaa7d8689590027fc4b66e96a0571b1a3210.

--- backend/server.js
+++ backend/server.js
@@ -26,42 +26,39 @@ if (process.env.NODE_ENV == "production") {
   app.use(express.static(path.join(__dirname1, "frontend/build")));
 
   app.get("*", (req, res) => {
-    db.sequelize
-    .sync({ alter:true})
-    .then(() => {
-      console.log("Synced db.");
-      /**
-       * Creates admin user if it doesn't exist
-       */
-      const adminData = {
-        username: "Admin",
-        password: "$2a$10$GcLCquYT3uW86bodXAH2euB2Ox.GmAIlbU/SWNfh94aNkQC2gYv4C",
-        isAdmin: true,
-      };
-      const admin = db.users
-        .findOne({
-          where: {
-            username: "Admin",
-            isAdmin: true,
-          },
-        })
-        .then((a) => {
-          if (!a) {
-            db.users.create(adminData);
-          }
-        });
-        
-      res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
-    })
-    .catch((err) => {
-      res.send("Database is down, try again later");
-    });
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
   });
-  
-  
 } else {
 }
 
+db.sequelize
+  .sync({ alter:true})
+  .then(() => {
+    console.log("Synced db.");
+    /**
+     * Creates admin user if it doesn't exist
+     */
+    const adminData = {
+      username: "Admin",
+      password: "$2a$10$GcLCquYT3uW86bodXAH2euB2Ox.GmAIlbU/SWNfh94aNkQC2gYv4C",
+      isAdmin: true,
+    };
+    const admin = db.users
+      .findOne({
+        where: {
+          username: "Admin",
+          isAdmin: true,
+        },
+      })
+      .then((a) => {
+        if (!a) {
+          db.users.create(adminData);
+        }
+      });
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
 
 app.set("port", process.env.PORT || 5000);
 const PORT = process.env.PORT || 5000;
